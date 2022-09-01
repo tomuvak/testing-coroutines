25da0d13dd4928fdcfe7196a85189629ad98a3ac
Tom Yuval <tomuvak@gmail.com>
2022-09-01 20:11:12 +0200

    (JVM, Native) AsyncTests (style): pass block directly rather than wrapping it in a lambda

--- src/jvmMain/kotlin/AsyncTests.kt
+++ src/jvmMain/kotlin/AsyncTests.kt
@@ -3,4 +3,4 @@ package com.tomuvak.testing.coroutines
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
-actual fun asyncTest(block: suspend CoroutineScope.() -> Unit): Unit = runBlocking { block() }
+actual fun asyncTest(block: suspend CoroutineScope.() -> Unit): Unit = runBlocking(block=block)
